fd7a9b928e4e62beec40fdb7dc99b42bcf391fcc
hjd219 <hapuha21@gmail.com>
2026-09-12 00:57:44 +0900

    Load common site shell on calculator pages

--- assets/analytics.js
+++ assets/analytics.js
@@ -13,4 +13,12 @@
   script.async = true;
   script.src = 'https://www.googletagmanager.com/gtag/js?id=' + encodeURIComponent(measurementId);
   document.head.appendChild(script);
+
+  /* 계산기 페이지도 메인과 동일한 공통 헤더·모바일 메뉴 사용 */
+  if (location.pathname === '/acquisition-calculator.html' || location.pathname === '/corporate-calculator.html') {
+    const shell = document.createElement('script');
+    shell.src = '/assets/site-shell.js?v=20260912-calculator-header';
+    shell.defer = true;
+    document.head.appendChild(shell);
+  }
 })();
