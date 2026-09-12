c4168baebbc23d2b87cc8ac5dbff0734ad274f0e
hjd219 <hapuha21@gmail.com>
2026-09-13 00:50:08 +0900

    계산기 일회성 수정 스크립트 정리

--- scripts/fix-calculator-display-target-once.mjs
+++
@@ -1,9 +0,0 @@
-import fs from 'node:fs';
-const p='index.html';
-let s=fs.readFileSync(p,'utf8');
-const old="document.querySelectorAll('#calculator .calculator-icon text:first-of-type')";
-const next="document.querySelectorAll('#calculator .calculator-icon > text:first-of-type')";
-if((s.split(old).length-1)!==1) throw new Error('display selector target mismatch');
-s=s.replace(old,next);
-fs.writeFileSync(p,s);
-console.log('calculator animation now targets display text only');
